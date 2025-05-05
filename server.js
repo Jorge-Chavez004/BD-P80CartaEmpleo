1157422dee1934b9b9ca2987a5e66bc5f3aa9888
Jorge_Chavez <mamimikisuu004@gmail.com>
2025-05-05 13:52:32 -0500

    Se modificado el endpoint de investigaciones 2

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/investigaciones/:carrera', async (req, res) => {
 
     try {
         const result = await pool.query(
-            'SELECT * FROM Investigacion WHERE carrera = $1',
+            'SELECT lineas_investigacion FROM Investigacion WHERE carrera = $1',
             [carrera]
         );
         
